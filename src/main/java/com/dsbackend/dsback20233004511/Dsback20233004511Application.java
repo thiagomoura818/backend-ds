f5880626e08dd8ef7ea84747e8cc0b59b66ad114
Thiago Moura <waatsaaynew@gmail.com>
2025-06-19 15:11:30 -0300

    primeiro commit

---
+++ src/main/java/com/dsbackend/dsback20233004511/Dsback20233004511Application.java
@@ -0,0 +1,13 @@
+package com.dsbackend.dsback20233004511;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Dsback20233004511Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Dsback20233004511Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/dsbackend/dsback20233004511/Dsback20233004511ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dsbackend.dsback20233004511;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Dsback20233004511ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
